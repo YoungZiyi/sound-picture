691213d813f5e9054361cdb510eabc78a9b786b3
YangZiyi <ziyi93@yeah.net>
2015-11-29 10:55:52 +0800

    Second commit

--- bxtcommon.py
+++ bxtcommon.py
@@ -7,6 +7,68 @@ SERVER_PORT = 10002
 CLIENT_HOST = '127.0.0.1'
 CLIENT_PORT = 10003
 
+
+JBT0_IP = "10.0.0.100"
+ICT_IP = "10.0.0.101"
+
+IST_HANDSHAKE = "51 c0 00 11"
+"51 0x xx crc"
+"51 c0 01 12"
+IST_SENDITEM = "51 c1 00 12"
+"51 c1 01 13"
+"51 c1 02 14"
+"51 c1 03 15"
+"51 c2 00 13"
+"51 c2 10 23"
+"51 c2 01 14"
+"51 c2 02 15"
+"51 c2 03 16"
+"51 c4 00 15"
+
+
+
+EVENT_SHAKEHANDS_RES = "52 c0 00 12"
+EVENT_SETWIDTH_RES = "52 0x xx crc"
+EVENT_RESET_STATUS_RES = "52 0x xx crc"
+EVENT_AVAILABLE = "52 c1 f0 03"
+EVENT_GETITEM_FINISHED = "52 c1 f1 04"
+EVENT_SENDITEM_FINISHED = "52 c1 f2 05"
+EVENT_HUANCUNJI_GETITEM_FINISHED = "52 c2 00 14"
+EVENT_READYFOR_GETITEM_OK = "52 c3 04 19"
+EVENT_READYFOR_GETITEM_NG = "52 c3 05 1a"
+EVENT_HUANCUNJI_BUSY = "52 c3 06 1b"
+EVENT_HUANCUNJI_GETITEM_ERROR = "52 c3 07 1c"
+EVENT_READYFOR_GETITEM_MIDDLE = "52 c2 01 15"
+EVENT_READYFOR_GETITEM_LEFT = "52 c2 02 16"
+EVENT_READYFOR_GETITEM_RIGHT = "52 c2 03 17"
+EVENT_TESTING = "52 c1 f3 06"
+EVENT_READYFOR_SENDITEM_OK = "52 c1 f4 07"
+EVENT_READYFOR_SENDITEN_NG = "52 c1 f5 08"
+EVENT_READYFOR_SENDITEM = "52 c1 f6 09"
+EVENT_DEVICE_WARNING_1 = "52 c3 xx crc"
+EVENT_DEVICE_WARNING_2 = "52 c3 00 15"
+EVENT_DEVICE_WARNING_3 = "52 c3 01 16"
+EVENT_DEVICE_WARNING_4 = "52 c3 02 17"
+EVENT_HUANCUNJI_NOTREADY = "52 c3 03 18"
+
+# device common status
+EVENT_COMMON_AVAILABLE = "52 c4 01 17"
+EVENT_COMMON_GETITEM_FINISHED = "52 c4 02 18"
+EVENT_COMMON_TESTING = "52 c4 03 19"
+EVENT_COMMON_WARNING = "52 c4 04 1a"
+EVENT_COMMON_READYFOR_SENDITEM = "52 c4 05 1b"
+EVENT_COMMON_SENDITEM_FINISHED = "52 c4 06 1c"
+EVENT_COMMON_BUSY = "52 c4 07 1d"
+
+STATUS_AVAILABLE = 0;
+STATUS_WAITING_FOR_NEXT_DEVICE_TO_BE_AVAILABLE= 2;
+STATUS_BUSY = 3;
+STATUS_BROKEN = 4;
+STATUS_RECVING = 5;
+STATUS_SENDING = 6;
+
+
+
 def sendToPeer(ip, port, content):
 	try:
 		theSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,19 +97,13 @@ def recvFromPeer(listenSocket):
 	return response
 
 class Device:
-	def __init__(self, name):
+	def __init__(self, name, ip):
 		self.name = name
-		self.item = None
-	def pushItem(self, item):
-		self.item = item
-	def popItem(self):
-		i = self.item
-		self.item = None
-		return i
-	def peekItem(self):
-		return self.item
+		self.ip = ip
+		self.status = AVAILABLE;
 
-class Item:
-	def __init__(self, name, status):
-		self.name = name
-		self.status = status
+def Connect(first, second):
+	if(first):
+		first.next = second;
+	if(second != None):
+		second.previous = first;
--- server.py
+++ server.py
@@ -4,9 +4,26 @@ import socket
 import time
 from bxtcommon import *
 
-ft01 = Device("FT01")
-yizai = Device("YIZAI")
 
+device_jbt0 = Device("Jiebotai0", JBT0_IP)
+device_ict = Device("ict", ICT_IP)
+device_hcj = Device("hcj", HCJ_IP)
+device_jbt1 = Device("jbt1", JBT1_IP)
+device_ft = Device("ft", FT_IP)
+device_yz = Device("yz", YZ_IP)
+device_ngsbj = Device("ngsbj", NGSBJ_IP)
+device_oksbj = Device("oksbj", OKSBJ_IP)
+
+Connect(None, device_jbt0)
+Connect(device_jbt0, device_ict)
+Connect(device_ict, device_hcj)
+Connect(device_hcj, device_jbt1)
+Connect(device_jbt1, device_ft)
+Connect(device_ft, device_yz)
+Connect(device_yz, device_ngsbj)
+Connect(device_yz, device_oksbj)
+Connect(device_ngsbj, None)
+Connect(device_oksbj, None)
 
 def main():
 	count = 0
@@ -21,32 +38,73 @@ def main():
 				continue
 			if(event == "reset"):
 				break
-			if ('FT01_TestFinish' in event):
-				count += 1
-				oldItem = ft01.peekItem()
-				if(oldItem != None):
-					sendToPeer(CLIENT_HOST, CLIENT_PORT, "FT01_HasTwoItems!")
-					continue;
 
-				newItem = Item("Item:"+str(count), "")
-				if("OK" in event):
-					newItem.status = "OK"
-				elif("NG" in event):
-					newItem.status = "NG"
+			remote_ip = getIpFromsocket();
+			current_device = getDeviceByIP(remote_ip)
+			# 握手反馈
+			if (event == EVENT_SHAKEHANDS_RES):
+				current_device.status = STATUS_AVAILABLE
+			# 调宽反馈
+			elif (event == EVENT_SETWIDTH_RES):
+				current_device.status = STATUS_AVAILABLE
+			# 复位后返回当前状态
+			elif (event == EVENT_RESET_STATUS_RES):
+				current_device.status = STATUS_AVAILABLE
+			# 空闲/正常 or 送板完成
+			elif(event == EVENT_AVAILABLE or event == EVENT_COMMON_AVAILABLE or event == EVENT_COMMON_SENDITEM_FINISHED or event == EVENT_SENDITEM_FINISHED)
+				current_device.status = STATUS_AVAILABLE;
+				previous_device = current_device.previous;
+				if(previous_device.status == STATUS_WAITING_FOR_NEXT_DEVICE_TO_BE_AVAILABLE):
+					sendToPeer(previous_device, IST_SENDITEM)
+					previous_device.status = STATUS_SENDING
+					current_device.status = STATUS_RECVING
+			# 接板完成
+			elif (event == EVENT_GETITEM_FINISHED):
+				current_device.status = STATUS_BUSY
+			# 缓存机接板完成
+			elif (event == EVENT_HUANCUNJI_GETITEM_FINISHED):
+				current_device.status = STATUS_BUSY
+			# 准备好接OK板
+			elif (event == EVENT_READYFOR_GETITEM_OK):
+				current_device.status = STATUS_AVAILABLE
+				previous_device = current_device.previous
+			# 准备好接NG板
+			# 缓存机接板忙
+			# 缓存机接板故障
+			# 已到达中间准备好接板
+			# 已到达左端准备好接板
+			# 已到达右端准备好接板
+			# 测试中
+
+			
+			# 测试OK，准备送板
+			elif (event == EVENT_READYFOR_SENDITEM_OK):
+				next_device = current_device.next
+				if(next_device.status == STATUS_AVAILABLE):
+					sendToPeer(current_device, HUANCUNJI_GETITEM_OK)
 				else:
-					sendToPeer(CLIENT_HOST, CLIENT_PORT, "FT01_ItemInWrongStatus!")
-					continue;	
-				#Put a new item with FT01
-				ft01.pushItem(newItem)
-				response = "YIZAI_Move_LEFT"
-			elif ("YIZAI_MoveFinish" in event):
-				item = yizai.peekItem()
-				if(item == None):
-					response = "YIZAI_PrepareGetItem"
-				elif(item.status == "OK"):
-					response = "OKSBJ_PrepareGetItem"
-				elif(item.status == "NG"):
-					response = "NGSBJ_PrepareGetItem"	
+					current_device.status = STATUS_WAITING_FOR_NEXT_DEVICE_TO_BE_AVAILABLE
+			# 测试NG，准备送板
+			elif (event == EVENT_READYFOR_SENDITEM_NG):
+				next_device = current_device.next
+				if(next_device.status == STATUS_AVAILABLE):
+					sendToPeer(current_device, HUANCUNJI_GETITEM_NG)
+				else:
+					current_device.status = STATUS_WAITING_FOR_NEXT_DEVICE_TO_BE_AVAILABLE
+			
+
+			# 准备送板
+			elif (event == EVENT_COMMON_READYFOR_SENDITEM):
+				next_device = current_device.next
+				if(next_device.status == STATUS_AVAILABLE):
+					sendToPeer(current_device, IST_SENDITEM)
+					current_device.status = STATUS_SENDING
+					next_device.status = STATUS_RECVING
+				else:
+					current_device.status = STATUS_WAITING_FOR_NEXT_DEVICE_TO_BE_AVAILABLE
+
+
+
 			elif event == "YIZAI_PrepareGetItemFinish":
 				response = "FT01_GiveItem"
 			elif event == "FT01_GiveItemFinish":
@@ -63,6 +121,7 @@ def main():
 			else:
 				response = "UNKNOWN MESSAGE: "
 				response += event
+
 
 			sendToPeer(CLIENT_HOST, CLIENT_PORT, response)
 		except Exception, e:
