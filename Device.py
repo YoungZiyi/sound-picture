0927a05ca72dd897ae83a6fef0447d73f411ca29
Ma Chi <hellomarch@gmail.com>
2015-12-06 20:36:08 +0800

    Almost finished

--- Device.py
+++ Device.py
@@ -3,25 +3,7 @@
 import time
 from Message import *
 
-#最低8bit标识设备的状态
-S_IDLE			=	0x0000	# 空闲
-S_MOVING		=	0x0001	# 移栽机在移动中
-S_RECVING		=	0x0002 	# 接板中
-S_WITH_ITEM		=	0x0003	# 设备上有板子
-S_TESTING		=	0x0004	# 测试中
-S_WAITING		=	0x0005	# 测试/接板 完成，等待下一个设备空闲以便送版
-S_SENDING		=	0x0006	# 送板中
-S_BROKEN		=	0x0007	# 故障状态
-S_RESETTING		=	0x0008	# 重启中
-S_MASK			=	0x000F	# Mask
-#最后一个标识代码逻辑有问题
-S_UNKNOWN		=	0xFFFF	# 未知状态
 
-#所携带的板子的状态
-ITEM_STATUS_OKAY		=	0
-ITEM_STATUS_NG			=	1
-ITEM_STATUS_UNKNOWN		=	2
-#ITEM_STATUS_NO_ITEM		=	3
 
 def matchStatus(statusA, statusB):
 	if(statusB <= S_MASK):
@@ -41,31 +23,58 @@ class Device:
 	def __init__(self, name, ip):
 		self.name = name
 		self.ip = ip
+		self.Reset()
+		
+	def Reset(self):
 		self.sock = None
-		self.status = S_AVAILABLE
+		self.status = S_AVAILABLE		#TODO 什么时候才能让Device可用? 还是一开始就可用?
 		self.status_start_time = time.time()
 		self.item_status = ITEM_STATUS_UNKNOWN
-
+		self.prepare_count = 0	
+		
 	def ChangeStatusTo(self, status):
 		self.status = status
 		self.status_start_time = time.time()
+		if(self.status == S_READY_TO_RECV_ITEM):
+			self.prepare_count = 0
 
 	# ng is a bool value
 	def ChangeItemStatusTo(self, status):
 		self.item_status = status
 
-	def SendInstruction(self, instruction):
+	def _SendInstruction(self, instruction):
 		if(not self.sock):
 			raise ExceptionCommunication(self.name+" No sock")
 		self.sock.send(instruction)
-		if(instruction == INSTRUCTION_DEVICE_SENDITEM):
-			self.status = S_SENDING
+
+	def SendInstruction(self, instruction):
+		if(instruction in [INSTRUCTION_DEVICE_SENDITEM, INSTRUCTION_YZ_MOVE_RIGHT_AND_RECV_ITEM, INSTRUCTION_HCJ_RECV_ITEM_OK, INSTRUCTION_HCJ_RECV_ITEM_NG]):
+			raise Exception("Please call the Specific SendInstrctionXXX")
+		_SendInstruction(self, instruction)
 	
+	def SendInstructionSendItem(self, instruction):
+		_SendInstruction(INSTRUCTION_DEVICE_SENDITEM)
+		if(self != device_yz):
+			#如果当前设备是移栽机, 则不用设置接板机的状态
+			self.next.ChangeStatusTo(S_RECVING)
+		self.ChangeStatusTo(S_SENDING)
+		#TODO 这个时候就开始传递Item的状态, 是不是太早了?
+		self.next.ChangeItemStatusTo(self.item_status)
+		self.ChangeItemStatusTo(ITEM_STATUS_UNKNOWN)
+		
+	def SendInstructionPrepareToRecvItem(self):
+		if(self == device_yz):
+			_SendInstruction(INSTRUCTION_YZ_MOVE_RIGHT_AND_RECV_ITEM)
+		elif(self == device_hcj):
+			_SendInstruction(self.previous.item_status == ITEM_STATUS_OK ? INSTRUCTION_HCJ_RECV_ITEM_OK : INSTRUCTION_HCJ_RECV_ITEM_NG)
+		#注意对于移栽机来说, 可能连续两次调用本函数分别达到S_HALF_READY_TO_RECV_ITEM, S_READY_TO_RECV_ITEM状态
+		self.status = S_PREPARING_TO_RECV
+		self.prepare_count = self.prepare_count +1
+		
 #这里开始我们定义所有机器, 并放到IP_Device_Map中
 device_jbt0 = Device("Jiebotai0", JBT0_IP)
 device_ict = Device("ict", ICT_IP)
 device_hcj = Device("hcj", HCJ_IP)
-device_jbt1 = Device("jbt1", JBT1_IP)
 device_ft = Device("ft", FT_IP)
 device_yz = Device("yz", YZ_IP)
 device_ngsbj = Device("ngsbj", NGSBJ_IP)
--- MessageHandler.py
+++ MessageHandler.py
@@ -12,37 +12,118 @@ def handle_msg(current_device, event):
 		next_device = current_device.next
 		
 		# TODO STATUS_BUSY
-		if (event == STATUS_BUSY):
-			pass
-
-		# Handle message sent from Jiebotai0
+		if (event == RES_STATUS_BUSY):
+			if(current_device not in [device_sbjok, device_sbjng]):
+				#RES_STATUS_BUSY is discarded 
+				return
+				
+		
 		if (current_device == device_jbt0):
+			# Handle message sent from Jiebotai0
 			if (event == EVENT_AVAILABLE):
-				current_device.ChangStatusTo(S_IDLE)
+				current_device.ChangeStatusTo(S_IDLE)
 			elif (event == EVENT_GETITEM_FINISHED):
-				current_device.ChangStatusTo(S_WITH_ITEM)
+				current_device.ChangeStatusTo(S_WITH_ITEM)
 			elif (event == EVENT_READYFOR_SENDITEM):
-				current_device.ChangStatusTo(S_WAITING)
+				current_device.ChangeStatusTo(S_READY_TO_SEND_ITEM)
 				if (matchStatus(next_device.status, S_IDLE)):
-					current_device.SendInstruction(INSTRUCTION_DEVICE_SENDITEM)
+					current_device.SendInstructionSendItem()
 			elif (event == EVENT_SENDITEM_FINISHED):
-				current_device.ChangStatusTo(S_IDLE)
-
-		if (current_device == device_ict):
+				current_device.ChangeStatusTo(S_IDLE)
+			else:
+				print "Event [%s] is not recognized for device [%s]" % [event, current_device.name]
+		elif (current_device in [device_ict, device_ft]):
 			if (event in [EVENT_AVAILABLE, EVENT_SENDITEM_FINISHED, RES_STATUS_AVAILABLE, RES_STATUS_SENDITEM_FINISHED]):
-				if (matchStatus(previous_device.status, S_WAITING)):
-					previous_device.SendInstruction(INSTRUCTION_DEVICE_SENDITEM)
+				current_device.ChangeStatusTo(S_IDLE)
+				if (matchStatus(previous_device.status, S_READY_TO_SEND_ITEM)):
+					previous_device.SendInstructionSendItem()
 			elif (event in [EVENT_GETITEM_FINISHED, EVENT_TESTING]):
-				current_device.ChangStatusTo(S_TESTING)
-			elif (event == EVENT_READYFOR_SENDITEM_OK):
-				current_device.ChangStatusTo(S_WAITING)# the status must specify the item's testing result OK
-				current_device.changItemStatusTo(ITEM_STATUS_OKAY)
-				if (matchStatus(next_device.status, S_IDLE)):	#此处我们暂不实现在缓存机的优化
-					current_device.SendInstruction(INSTRUCTION_DEVICE_SENDITEM)
-			elif (event == EVENT_READYFOR_SENDITEM_NG):
-				current_device.ChangStatusTo(S_WAITING)# the status must specify the item's testing result NG
-				if (matchStatus(next_device.status, S_IDLE)):
-					current_device.SendInstruction(INSTRUCTION_DEVICE_SENDITEM)
-					current_device.ChangStatusTo(S_SENDING)
-			elif (event == EVENT_SENDITEM_FINISHED):
-				current_device.
+				current_device.ChangeStatusTo(S_TESTING)
+			elif (event in [EVENT_READYFOR_SENDITEM_OK, EVENT_READYFOR_SENDITEM_NG]):
+				# 测试机器已经测试完毕一块板子
+				if(current_device.status == S_READY_TO_SEND_ITEM):
+					#上一块板子还没送出去
+					print "WARN: One more item testing finished when the last item has not been sent in device [%s]" % current_device.name
+				current_device.ChangeStatusTo(S_READY_TO_SEND_ITEM)
+				current_device.changItemStatusTo(event == EVENT_READYFOR_SENDITEM_OK ? ITEM_STATUS_OK : ITEM_STATUS_NG)
+				if (next_device.status in [S_IDLE, S_PREPARING_TO_RECV, S_HALF_READY_TO_RECV_ITEM]):
+					if(next_device.status == S_PREPARING_TO_RECV):
+						print "WARN: YZ is preparing right now"
+					# 移栽机空闲之后会处于S_HALF_READY_TO_RECV_ITEM状态, 以节省时间.
+					# 此处我们暂不实现在缓存机的优化
+					next_device.SendInstructionPrepareToRecvItem()
+				elif (next_device.status == S_READY_TO_RECV_ITEM):
+					'''
+						测试机刚刚测试好, 还没发指令让下一个机器准备, 下一个机器就已经准备好了, 那就直接送板吧.
+						这种情况可能发生在如下场景:
+						1. 前面的移栽机/缓存机正在准备接板, 
+						2. 测试机上的板子被人手工拿走了
+						3. 测试机给服务器发送一个"送板完成"的消息, 
+						4. 服务器指挥测试机继续测试, 
+						5. 移栽机/缓存机准备完毕, 
+						6. 测试机测试完毕, 发现前面的移栽机/缓存级已经准备好了
+						TODO 需要确认该场景的可能性, 以及item_status在传递的时候能否保持正确.
+					'''
+					current_device.SendInstructionSendItem()
+			else:
+				print "Event [%s] is not recognized for device [%s]" % (event, current_device.name)
+		elif (current_device == device_hcj):
+			pass
+		elif (current_device == device_yz):
+			if(event == EVENT_READYFOR_GETITEM_RIGHT):
+				#移栽机报告已经到达右边
+				if(current_device.status != S_PREPARING_TO_RECV):
+						print "WARN: yz [%s] report EVENT_READYFOR_GETITEM_RIGHT from status [%d]" % (current_device.name, current_device.status)
+				if(current_device.prepare_count == 1):
+					#第一次到达算HALF_READY, 第二次才算READY
+					current_device.ChangeStatusTo(S_HALF_READY_TO_RECV_ITEM)
+					if(previous_device.status == S_READY_TO_SEND_ITEM):
+						#后面的机器在等待, 需要马上来第二次
+						current_device.SendInstructionPrepareToRecvItem()
+				elif(current_device.prepare_count == 2):
+					#移栽机第二次报告已经到达右边
+					current_device.ChangeStatusTo(S_READY_TO_RECV_ITEM)
+					if(previous_device.status != S_READY_TO_SEND_ITEM):
+						#TODO 确认下如果后面的机器没有READY_TO_SEND, 怎么处理?
+						print "WARN: yizaiji is ready to recv item, but the FT is not ready to send item. anybody steal the item?"
+					else:
+						#让后面的机器送板
+						previous_device.SendInstructionSendItem()
+			elif(event==EVENT_GETITEM_FINISHED):
+				#移栽机收到板子, 默认送去NG收板机
+				cmd = INSTRUCTION_YZ_MOVE_LEFT_AND_SEND_ITEM
+				if(current_device.item_status == ITEM_STATUS_OK):
+					cmd = INSTRUCTION_YZ_MOVE_RIGHT_AND_SEND_ITEM 
+				current_device.SendInstruction(cmd)
+				current_device.ChangeStatusTo(S_SENDING)
+			elif(event==EVENT_SENDITEM_FINISHED):
+				if(current_device.status != S_SENDING):
+					print "WARN: [%s]'s status jumped from [%d] to S_IDLE" %(current_device.name, current_device.status)
+				current_device.ChangeStatusTo(S_IDLE)
+				#移栽机把板子送走了, 应该让它马上准备接板, 以优化效率
+				current_device.SendInstructionPrepareToRecvItem()
+		elif (current_device in [device_sbjok, device_sbjng]):
+			if(event == RES_STATUS_BUSY):
+				#TODO 收板机最好不要只给BUSY指令, 而是给一个开始收板指令
+				current_device.ChangeStatusTo(S_WITH_ITEM)
+			elif(event == EVENT_AVAILABLE):
+				current_device.ChangeStatusTo(S_IDLE)
+			else:
+				print "Event [%s] is not recognized for device [%s]" % [event, current_device.name]
+		else:
+			print "Device is not recognized %s" % device.name
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
